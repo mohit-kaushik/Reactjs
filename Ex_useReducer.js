a3859ea6a9d2dab887b7d16e4175bc4bdacfdb48
Mohit Kaushik <31967869+mohit-kaushik@users.noreply.github.com>
2023-05-28 15:48:56 +0530

    more readable Ex_useReducer.js

--- Ex_useReducer.js
+++ Ex_useReducer.js
@@ -1,6 +1,8 @@
 let def = `
-In React, the `useReducer` hook is a built-in hook that allows you to manage state in your functional components using a reducer pattern. It is an alternative to the `useState` hook and provides a way to handle complex state transitions in a more structured manner.
-The `useReducer` hook takes two arguments: a reducer function and an initial state. The reducer function is responsible for specifying how the state should change based on the dispatched actions.
+In React, the `useReducer` hook is a built-in hook that allows you to manage state in your functional components using a reducer pattern. 
+It is an alternative to the `useState` hook and provides a way to handle complex state transitions in a more structured manner.
+The `useReducer` hook takes two arguments: a reducer function and an initial state. The reducer function is responsible for specifying 
+how the state should change based on the dispatched actions.
 Here's an example of how you can use `useReducer` in React:
 `
 
@@ -39,13 +41,20 @@ const Counter = () => {
 export default Counter;
 
 
-let explain = `In the example above, we have a simple counter component. The initial state is defined as an object with a `count` property set to 0. The reducer function handles the state transitions based on the dispatched actions. In this case, the actions are `increment` and `decrement`, which modify the `count` property of the state.
+let explain = `In the example above, we have a simple counter component. The initial state is defined as an object with a `count` property 
+set to 0. The reducer function handles the state transitions based on the dispatched actions. In this case, the actions are `increment` 
+and `decrement`, which modify the `count` property of the state.
 
-Inside the `Counter` component, we call the `useReducer` hook and pass in the reducer function and the initial state. It returns an array with the current state and a dispatch function.
+Inside the `Counter` component, we call the `useReducer` hook and pass in the reducer function and the initial state. 
+It returns an array with the current state and a dispatch function.
 
-We render the current count value from the state and provide buttons that dispatch the appropriate actions when clicked. The dispatched actions are passed to the `dispatch` function, which triggers the state transition in the reducer.
+We render the current count value from the state and provide buttons that dispatch the appropriate actions when clicked. 
+The dispatched actions are passed to the `dispatch` function, which triggers the state transition in the reducer.
 
-When an action is dispatched, React will call the reducer function with the current state and the dispatched action. The reducer then determines the next state based on the action type and returns it. React will update the state and re-render the component accordingly.
+When an action is dispatched, React will call the reducer function with the current state and the dispatched action. 
+The reducer then determines the next state based on the action type and returns it. React will update the state and re-render the component 
+accordingly.
 
-This way, `useReducer` provides a clean and predictable way to manage state transitions in your React components, especially when the state logic becomes more complex.
+This way, `useReducer` provides a clean and predictable way to manage state transitions in your React components, 
+especially when the state logic becomes more complex.
 `

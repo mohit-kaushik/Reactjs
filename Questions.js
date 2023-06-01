06dbc9051c33512c27da42658c0350fc915adce2
Mohit Kaushik <31967869+mohit-kaushik@users.noreply.github.com>
2023-06-01 23:17:04 +0530

    Update Questions.js

--- Questions.js
+++ Questions.js
@@ -3,3 +3,9 @@ it performs a shallow comparison by default. If the updated state object is the 
 React assumes that the state hasn't changed, even if the properties within the object have been modified. 
 As a result, React may skip the re-rendering of your component, leading to 
 inconsistencies in the UI.
+
+Q. What is advantage of redux over useContext?
+Redux is a state management system used for cross-component or app-wide state. While React Context can also manage state, Redux offers some advantages. 
+React Context can become complex, especially in large applications, with multiple Context Providers and deeply nested JSX code. Additionally, 
+React Context may not perform well for high-frequency state changes. 
+These limitations led to the development of Redux, which is a flux-like state management library and addresses these disadvantages.

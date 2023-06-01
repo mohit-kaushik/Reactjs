1bc8ef8f48dd0c5f305de18afd21b0ca8b2d6751
Mohit Kaushik <31967869+mohit-kaushik@users.noreply.github.com>
2023-06-01 23:34:48 +0530

    Update overview.js

--- Redux/overview.js
+++ Redux/overview.js
@@ -1,1 +1,8 @@
+let overview = `Redux is a state management system that involves having a central data store for an entire application. 
+This store contains the state, including authentication, theming, and user input. Components subscribe to the store to receive updates when the data changes, 
+allowing them to react accordingly and update the UI. However, components are not allowed to directly manipulate the store data. 
+Instead, reducer functions are used to mutate the store data based on actions dispatched by components. Actions are JavaScript objects that describe 
+the desired operation, and the reducer performs the operation and produces a new state. When the state in the data store is updated, subscribing components 
+are notified to update their UI. 
+The passage introduces these concepts and highlights that the upcoming modules will delve into the implementation details.`
 
